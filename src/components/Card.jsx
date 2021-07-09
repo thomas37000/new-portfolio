967648b706148f69a16bed13429addbbb49926a0
Thomas <thomas@avecnous.eu>
2021-07-09 12:13:00 +0200

    add components folder

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Card = (props) => {
+  return <div>hello world</div>;
+};
+
+// Card.propTypes = {
+
+// };
+
+export default Card;
