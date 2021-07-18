d224764cc07a4f827645641e9a3146a5383c6562
Thomas <thomas@avecnous.eu>
2021-07-18 17:02:08 +0200

    begin the grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import Card from './components/Card';
+import Grid from './components/Grid';
 
 function App() {
   return (
     <div className='App'>
       <Nav />
       <Card />
+      <Grid />
     </div>
   );
 }
