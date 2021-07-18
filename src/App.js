ebf80314245b5872477a074f9f1e5937062bd7a5
Thomas <thomas@avecnous.eu>
2021-07-18 16:08:17 +0200

    begin my description

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-      </header>
+      <header className='App-header'></header>
       <Card />
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import logo from '../../src/logo.svg';
+import './Card.css';
 // import PropTypes from 'prop-types';
 
 const Card = (props) => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <div className='description'>
+        <div className='headerDesc'>
+          Bonjour je suis Thomas Chalanson,{' '}
+          <h1>Dévellopeur Web Javascript - Front End</h1>
+        </div>
+        <div>
+          Je travaille avec la bibliothèque <h2>React</h2>
+          <img src={logo} className='App-logo' alt='logo' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // Card.propTypes = {
